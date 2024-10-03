f067482b6305f4708660e2df4c9c2b1d9687a52d
Augusto Rodriguez <augustom.rodriguezc@hotmail.com>
2024-10-02 22:28:53 -0300

    ADD: Agrego resolución Punto_C.

--- RTA_SCRIPT_Examen_20241001/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241001/Punto_C.sh
@@ -1,17 +1,17 @@
-groupadd p1c2_2024_gAlumno
-groupadd p1c2_2024_gProfesores
-useradd -m -s /bin/bash -c Alumno 1 -G p1c2_2024_gAlumno p1c2_2024_A1
-useradd -m -s /bin/bash -c Alumno 2 -G p1c2_2024_gAlumno p1c2_2024_A2
-useradd -m -s /bin/bash -c Alumno 3 -G p1c2_2024_gAlumno p1c2_2024_A3
-useradd -m -s /bin/bash -c Profesor 1 -G p1c2_2024_gProfesores p1c2_2024_P1
+sudo groupadd p1c2_2024_gAlumno
+sudo groupadd p1c2_2024_gProfesores
+sudo useradd -m -s /bin/bash -c Alumno 1 -G p1c2_2024_gAlumno p1c2_2024_A1
+sudo useradd -m -s /bin/bash -c Alumno 2 -G p1c2_2024_gAlumno p1c2_2024_A2
+sudo useradd -m -s /bin/bash -c Alumno 3 -G p1c2_2024_gAlumno p1c2_2024_A3
+sudo useradd -m -s /bin/bash -c Profesor 1 -G p1c2_2024_gProfesores p1c2_2024_P1
 grep p1c2_2024_A1 /etc/shadow | awk -F ':' '{print }'
 grep p1c2_2024_A2 /etc/shadow | awk -F ':' '{print }'
 grep p1c2_2024_A3 /etc/shadow | awk -F ':' '{print }'
-chown p1c2_2024_A1 /Examenes-UTN/alumno_1
-chmod 750 /Examenes-UTN/alumno_1
-chown p1c2_2024_A2 /Examenes-UTN/alumno_2
-chmod 760 /Examenes-UTN/alumno_2
-chown p1c2_2024_A3 /Examenes-UTN/alumno_3
-chmod 700 /Examenes-UTN/alumno_3
-chown p1c2_2024_P1 /Examenes-UTN/profesores
-chmod 775 /Examenes-UTN/profesores
+sudo chown p1c2_2024_A1 /Examenes-UTN/alumno_1
+sudo chmod 750 /Examenes-UTN/alumno_1
+sudo chown p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chmod 760 /Examenes-UTN/alumno_2
+sudo chown p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chmod 700 /Examenes-UTN/alumno_3
+sudo chown p1c2_2024_P1 /Examenes-UTN/profesores
+sudo chmod 775 /Examenes-UTN/profesores
