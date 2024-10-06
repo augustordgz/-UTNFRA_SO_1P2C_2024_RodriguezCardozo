700141d23ee2a5769e5be81a5e35e13b7641f36b
Augusto Rodriguez <augustom.rodriguezc@hotmail.com>
2024-10-06 14:34:10 -0300

    fix: Corrijo Punto_C

--- RTA_SCRIPT_Examen_20241001/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241001/Punto_C.sh
@@ -15,7 +15,15 @@ sudo chown p1c2_2024_A3 /Examenes-UTN/alumno_3
 sudo chmod 700 /Examenes-UTN/alumno_3
 sudo chown p1c2_2024_P1 /Examenes-UTN/profesores
 sudo chmod 775 /Examenes-UTN/profesores
-sudo -su -c whoami > /Examenes-UTN/alumno_1/validar.txt p1c2_2024_A1
-sudo -su -c whoami > /Examenes-UTN/alumno_2/validar.txt p1c2_2024_A2
-sudo -su -c whoami > /Examenes-UTN/alumno_3/validar.txt p1c2_2024_A3
-sudo -su -c whoami > /Examenes-UTN/profesores/validar.txt p1c2_2024_P1
+cd Examenes-UTN/alumno_1
+touch validar.txt
+cd Examenes-UTN/alumno_2
+touch validar.txt
+cd Examenes-UTN/alumno_3
+touch validar.txt
+cd Examenes-UTN/profesores
+touch validar.txt
+sudo su -c whoami > /Examenes-UTN/alumno_1/validar.txt p1c2_2024_A1
+sudo su -c whoami > /Examenes-UTN/alumno_2/validar.txt p1c2_2024_A2
+sudo su -c whoami > /Examenes-UTN/alumno_3/validar.txt p1c2_2024_A3
+sudo su -c whoami > /Examenes-UTN/profesores/validar.txt p1c2_2024_P1
