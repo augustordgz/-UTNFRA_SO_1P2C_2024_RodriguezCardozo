b496e78395dce1a18e28b61ed13353a959bb4692
Augusto Rodriguez <augustom.rodriguezc@hotmail.com>
2024-10-06 18:52:33 +0000

    ADD: Agrego resolución Punto_E

---
+++ RTA_SCRIPT_Examen_20241001/Punto_E.sh
@@ -0,0 +1,5 @@
+cd UTNFRA_SO_1P2C_2024_RodriguezCardozo/RTA_ARCHIVOS_Examen_20241001
+cat /proc/meminfo
+grep MemTotal proc/meminfo > Filtro_Basico.txt
+sudo dmidecode -t chassis
+sudo dmidecode -t chassis | grep Manufacturer >> Filtro_Basico.txt
