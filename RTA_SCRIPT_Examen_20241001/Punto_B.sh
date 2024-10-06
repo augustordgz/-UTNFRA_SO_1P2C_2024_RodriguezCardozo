56688e33ce974607500e98bc9ae5434d656c0a4f
Augusto Rodriguez <augustom.rodriguezc@hotmail.com>
2024-10-06 21:41:03 +0000

    FIX: Vuevlo a corregir el Punto_B

--- RTA_SCRIPT_Examen_20241001/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241001/Punto_B.sh
@@ -53,13 +53,23 @@ sudo mkfs -t ext4 /dev/sdc8
 sudo mkfs -t ext4 /dev/sdc9
 sudo mkfs -t ext4 /dev/sdc10
 sudo mkfs -t ext4 /dev/sdc11
- echo /dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc2 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc3 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc5 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc6 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc7 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc8 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc10 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
- echo /dev/sdc11 /Examenes-UTN/profesores ext4 defaults 0 0 | sudo tee -a /etc/fstab
+echo /dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc2 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a 
+echo /dev/sdc3 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc5 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc6 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc7 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc8 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc10 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
+echo /dev/sdc11 /Examenes-UTN/profesores ext4 defaults 0 0 | sudo tee -a /etc/fstab
+sudo mount -a
