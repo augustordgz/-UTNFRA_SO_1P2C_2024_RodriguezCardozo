a68b03d49e1d850777e4745a9a9eefb797c7fce6
Augusto Rodriguez <augustom.rodriguezc@hotmail.com>
2024-10-07 22:08:58 +0000

    fix: corrijo errores de tipeo en varios puntos

--- RTA_SCRIPT_Examen_20241001/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241001/Punto_C.sh
@@ -4,9 +4,9 @@ sudo useradd -m -s /bin/bash -c Alumno 1 -G p1c2_2024_gAlumno p1c2_2024_A1
 sudo useradd -m -s /bin/bash -c Alumno 2 -G p1c2_2024_gAlumno p1c2_2024_A2
 sudo useradd -m -s /bin/bash -c Alumno 3 -G p1c2_2024_gAlumno p1c2_2024_A3
 sudo useradd -m -s /bin/bash -c Profesor 1 -G p1c2_2024_gProfesores p1c2_2024_P1
-grep p1c2_2024_A1 /etc/shadow | awk -F ':' '{print }'
-grep p1c2_2024_A2 /etc/shadow | awk -F ':' '{print }'
-grep p1c2_2024_A3 /etc/shadow | awk -F ':' '{print }'
+grep "p1c2_2024_A1" /etc/shadow | awk -F ':' '{print }'
+grep "p1c2_2024_A2" /etc/shadow | awk -F ':' '{print }'
+grep "p1c2_2024_A3" /etc/shadow | awk -F ':' '{print }'
 sudo chown p1c2_2024_A1 /Examenes-UTN/alumno_1
 sudo chmod 750 /Examenes-UTN/alumno_1
 sudo chown p1c2_2024_A2 /Examenes-UTN/alumno_2
@@ -23,7 +23,7 @@ cd Examenes-UTN/alumno_3
 touch validar.txt
 cd Examenes-UTN/profesores
 touch validar.txt
-sudo su -c whoami > /Examenes-UTN/alumno_1/validar.txt p1c2_2024_A1
-sudo su -c whoami > /Examenes-UTN/alumno_2/validar.txt p1c2_2024_A2
-sudo su -c whoami > /Examenes-UTN/alumno_3/validar.txt p1c2_2024_A3
-sudo su -c whoami > /Examenes-UTN/profesores/validar.txt p1c2_2024_P1
+sudo su -c "whoami > /Examenes-UTN/alumno_1/validar.txt" p1c2_2024_A1
+sudo su -c "whoami > /Examenes-UTN/alumno_2/validar.txt" p1c2_2024_A2
+sudo su -c "whoami > /Examenes-UTN/alumno_3/validar.txt" p1c2_2024_A3
+sudo su -c "whoami > /Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
--- RTA_SCRIPT_Examen_20241001/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241001/Punto_E.sh
@@ -1,5 +1,6 @@
 cd UTNFRA_SO_1P2C_2024_RodriguezCardozo/RTA_ARCHIVOS_Examen_20241001
 cat /proc/meminfo
-grep MemTotal proc/meminfo > Filtro_Basico.txt
+grep "MemTotal" /proc/meminfo > Filtro_Basico.txt
 sudo dmidecode -t chassis
-sudo dmidecode -t chassis | grep Manufacturer >> Filtro_Basico.txt
+sudo dmidecode -t chassis | grep "Chassis" >> Filtro_Basico.txt
+sudo dmidecode -t chassis | grep "Manufacturer" >> Filtro_Basico.txt
